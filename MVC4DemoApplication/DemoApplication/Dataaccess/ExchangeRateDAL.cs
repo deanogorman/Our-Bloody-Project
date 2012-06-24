350c6e1600ef344769087165d10fccb5ed43cb7d
Dean O'Gorman <deanogorman@yahoo.com>
2012-06-24 21:34:12 +0100

    MVC4DemoApplication for testing

---
+++ MVC4DemoApplication/DemoApplication/Dataaccess/ExchangeRateDAL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DemoApplication.Models;
+
+namespace DemoApplication.Dataaccess
+{
+    public class ExchangeRateDAL
+    {
+        public ExchangeRate findExchangeRateForConverstion(String fromCurrency, String toCurrency)
+        {
+            // Normally we will do a lookup on the database, but not tonight!
+            return new ExchangeRate("USD", "EUR", 1.2333);
+        }
+
+        public ExchangeRate addNewExchangeRate(String fromCurrency, string toCurrency, double rate)
+        {
+            // This is where we typically new the class and add it to the database
+            return null;
+        }
+        public ExchangeRate deleteExchangeRate()
+        {
+            return null;
+        }
+    }
+}
---
+++ MVC4DemoApplication/DemoApplication/Models/ExchangeRate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Linq.Mapping;
+
+namespace DemoApplication.Models
+{
+    [Table (Name="EXCHANGERATES")] 
+    public class ExchangeRate
+    {
+        [Column(IsPrimaryKey = true)]
+        int id;
+        [Column(Name = "FROMCURRENCY")]
+        String fromCurrency;
+        [Column(Name = "TOCURRENCY")]
+        String toCurrency;
+        [Column(Name = "RATE")]
+        double rate;
+
+        public double Rate
+        {
+            get { return rate; }
+        }
+
+        public ExchangeRate(String _fromCurrency, String _toCurrency, double _rate)
+        {
+            this.fromCurrency = _fromCurrency;
+            this.toCurrency = _toCurrency;
+            this.rate = _rate;
+        }
+
+
+
+
+
+
+    }
+}
